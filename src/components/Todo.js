dd2668c4d6692ca448fa3fb0421050a0cffb8305
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-25 12:51:59 +0430

    change

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,28 @@
-function Todo(props) {
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
-  function deleteHandler(){
-    console.log("Clicked!");
-    console.log(props.text);
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
   }
-  
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
