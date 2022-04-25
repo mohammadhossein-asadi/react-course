9eb783b0040b44d848cf9a4431a42874a8e079b0
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-25 12:22:16 +0430

    create new file

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,10 @@
 function Modal() {
   return (
-
+    <div className="modal">
+      <p>Are you sure?</p>
+      <buttun className="btn btn--alt">Cancle</buttun>
+      <buttun className="btn">Confirm</buttun>
+    </div>
   );
 }
 
